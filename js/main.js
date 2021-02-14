e84beafffd464ce7ef61c2f42b413c7943aba95c
Cesar Celis <celis212@gmail.com>
2021-02-14 12:20:47 -0500

    footer almost done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-function darkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
+const darkButtonNav = document.querySelector("#nav-dark-mode")
+const darkButtonBurger = document.querySelector("#burger-dark-mode")
+darkButtonNav.addEventListener('click', () => {
+    document.body.classList.toggle('dark')
+})
+darkButtonBurger.addEventListener('click', () => {
+    document.body.classList.toggle('dark')
+})
